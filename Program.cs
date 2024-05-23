219d2a99b2a20c5c9fb67ae764dd3d214484524a
Joe Sun <dps_swanuk@hotmail.com>
2024-05-23 23:07:00 +0200

    upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace TotalBits;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Input [N]: ");
+        var N = Console.ReadLine();
+        var nInt = int.Parse(N!);
+        var total = 0;
+        for (int i = 1; i < nInt + 1; i++)
+        {
+            var str = Convert.ToString(i,2);
+            total += str.Count(c => c == '1');
+        }
+        Console.WriteLine($"Total bits between 1 to {N} is {total}");
+    }
+}
